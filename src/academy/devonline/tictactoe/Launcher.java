fe2d516ea2cd321942d6bc083de0704bb31324f8
EgorEko <asya.wear@gmail.com>
2022-01-09 10:40:36 +0200

    Convert CellNumberConverter class to interface

--- src/academy/devonline/tictactoe/Launcher.java
+++ src/academy/devonline/tictactoe/Launcher.java
@@ -1,6 +1,7 @@
 package academy.devonline.tictactoe;
 
 import academy.devonline.tictactoe.component.*;
+import academy.devonline.tictactoe.component.keypad.DesktopNumericKeypadCellNumberConverter;
 
 /**
  * @author devonline
@@ -9,7 +10,7 @@ import academy.devonline.tictactoe.component.*;
 public final class Launcher {
 
     public static void main(final String[] args) {
-        CellNumberConverter cellNumberConverter = new CellNumberConverter();
+        CellNumberConverter cellNumberConverter = new DesktopNumericKeypadCellNumberConverter();
         final Game game = new Game(
                 new DataPrinter(cellNumberConverter),
                 new ComputerMove(),
---
+++ src/academy/devonline/tictactoe/component/keypad/DesktopNumericKeypadCellNumberConverter.java
@@ -0,0 +1,34 @@
+package academy.devonline.tictactoe.component.keypad;
+
+import academy.devonline.tictactoe.component.CellNumberConverter;
+import academy.devonline.tictactoe.model.Cell;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ */
+public class DesktopNumericKeypadCellNumberConverter implements CellNumberConverter {
+
+    private final char[][] mapping = {
+            {'7', '8', '9'},
+            {'4', '5', '6'},
+            {'1', '2', '3'}
+    };
+
+    @Override
+    public Cell toCell(final char number) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (mapping[i][j] == number) {
+                    return new Cell(i, j);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public char toNumber(final Cell cell) {
+        return mapping[cell.getCol()][cell.getRow()];
+    }
+}
