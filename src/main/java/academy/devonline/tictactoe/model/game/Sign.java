6380a1b82354ae1127fe785800c9b766833e9115
EgorEko <asya.wear@gmail.com>
2022-02-08 15:42:16 +0200

    Add PreventUserWinComputerMoveStrategy

--- src/main/java/academy/devonline/tictactoe/model/game/Sign.java
+++ src/main/java/academy/devonline/tictactoe/model/game/Sign.java
@@ -13,6 +13,16 @@ public enum Sign {
 
     EMPTY;
 
+    public Sign oppositeSign() {
+        if (this == X) {
+            return O;
+        } else if (this == O) {
+            return X;
+        } else {
+            throw new IllegalStateException("Empty value does not have an opposite one!");
+        }
+    }
+
     @Override
     public String toString() {
         if (this == EMPTY) {
