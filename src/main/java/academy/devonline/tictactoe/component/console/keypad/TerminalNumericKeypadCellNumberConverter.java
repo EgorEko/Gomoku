9456cd01489eeeff3cba66af12c5deb11c41084e
EgorEko <asya.wear@gmail.com>
2022-02-08 06:00:12 +0200

    Fix toNumber and toCell methods for terminal impl

--- src/main/java/academy/devonline/tictactoe/component/console/keypad/TerminalNumericKeypadCellNumberConverter.java
+++ src/main/java/academy/devonline/tictactoe/component/console/keypad/TerminalNumericKeypadCellNumberConverter.java
@@ -3,35 +3,35 @@ package academy.devonline.tictactoe.component.console.keypad;
 import academy.devonline.tictactoe.component.console.CellNumberConverter;
 import academy.devonline.tictactoe.model.game.Cell;
 
+import static java.lang.String.format;
+
 /**
  * @author devonline
  * @link http://devonline.academy/java
  */
 public class TerminalNumericKeypadCellNumberConverter implements CellNumberConverter {
 
-    /*private final char[][] mapping = {
-            {'1', '2', '3'},
-            {'4', '5', '6'},
-            {'7', '8', '9'}
-    };*/
-
     @Override
     public Cell toCell(final char number) {
-        /*for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (mapping[i][j] == number) {
-                    return new Cell(i, j);
-                }
-            }
+        if (number >= '1' && number <= '9'){
+            final int val = number - '0' - 1;
+            return new Cell(val / 3, val % 3);
+        }else {
+            throw new IllegalArgumentException(
+                    format("Number parameter must be between '1' and '9'! Current value is '%s'!", number)
+            );
         }
-        return null;*/
-        final int val = number - '0' - 1;
-        return new Cell(val / 3, val % 3);
     }
 
     @Override
     public char toNumber(final Cell cell) {
-        /*return mapping[cell.getRow()][cell.getCol()];*/
-        return (char) ('0' + (cell.getRow() * 3 + cell.getCol() + 1));
+        if (cell.getRow() >= 0 && cell.getRow() <= 2 && cell.getCol() >= 0 && cell.getCol() <= 2) {
+            return (char) ('0' + (cell.getRow() * 3 + cell.getCol() + 1));
+        }else {
+            throw new IllegalArgumentException(
+                    format("Number parameter must be between '1' and '9'! Current value is '%s'!",
+                            cell.getRow(), cell.getCol())
+            );
+        }
     }
 }
