893eb93b73ab9dcbdb179717a1633babe848e9d8
EgorEko <asya.wear@gmail.com>
2022-02-08 13:57:47 +0200

    Add WinNowComputerMoveStrategy

---
+++ src/main/java/academy/devonline/tictactoe/component/strategy/WinNowComputerMoveStrategy.java
@@ -0,0 +1,112 @@
+package academy.devonline.tictactoe.component.strategy;
+
+import academy.devonline.tictactoe.component.ComputerMoveStrategy;
+import academy.devonline.tictactoe.model.game.Cell;
+import academy.devonline.tictactoe.model.game.GameTable;
+import academy.devonline.tictactoe.model.game.Sign;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ */
+public class WinNowComputerMoveStrategy implements ComputerMoveStrategy {
+    @Override
+    public boolean tryToMakeMove(final GameTable gameTable, final Sign sign) {
+        return tryToMakeMoveByRows(gameTable, sign) ||
+                tryToMakeMoveByCols(gameTable, sign) ||
+                tryToMakeMoveByMainDiagonal(gameTable, sign) ||
+                tryToMakeMoveBySecondaryDiagonal(gameTable, sign);
+    }
+
+    private boolean tryToMakeMoveByRows(GameTable gameTable, Sign sign) {
+        for (int i = 0; i < 3; i++) {
+            int countEmptyCells = 0;
+            int countSignCells = 0;
+            Cell lastEmptyCell = null;
+            for (int j = 0; j < 3; j++) {
+                final Cell cell = new Cell(i, j);
+                if (gameTable.isEmpty(cell)) {
+                    lastEmptyCell = cell;
+                    countEmptyCells++;
+                } else if (gameTable.getSign(cell) == sign) {
+                    countSignCells++;
+                } else {
+                    break;
+                }
+            }
+            if (countEmptyCells == 1 && countSignCells == 2) {
+                gameTable.setSign(lastEmptyCell, sign);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean tryToMakeMoveByCols(GameTable gameTable, Sign sign) {
+        for (int i = 0; i < 3; i++) {
+            int countEmptyCells = 0;
+            int countSignCells = 0;
+            Cell lastEmptyCell = null;
+            for (int j = 0; j < 3; j++) {
+                final Cell cell = new Cell(j, i);
+                if (gameTable.isEmpty(cell)) {
+                    lastEmptyCell = cell;
+                    countEmptyCells++;
+                } else if (gameTable.getSign(cell) == sign) {
+                    countSignCells++;
+                } else {
+                    break;
+                }
+            }
+            if (countEmptyCells == 1 && countSignCells == 2) {
+                gameTable.setSign(lastEmptyCell, sign);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean tryToMakeMoveByMainDiagonal(GameTable gameTable, Sign sign) {
+        int countEmptyCells = 0;
+        int countSignCells = 0;
+        Cell lastEmptyCell = null;
+        for (int j = 0; j < 3; j++) {
+            final Cell cell = new Cell(j, j);
+            if (gameTable.isEmpty(cell)) {
+                lastEmptyCell = cell;
+                countEmptyCells++;
+            } else if (gameTable.getSign(cell) == sign) {
+                countSignCells++;
+            } else {
+                break;
+            }
+        }
+        if (countEmptyCells == 1 && countSignCells == 2) {
+            gameTable.setSign(lastEmptyCell, sign);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean tryToMakeMoveBySecondaryDiagonal(final GameTable gameTable, final Sign sign) {
+        int countEmptyCells = 0;
+        int countSignCells = 0;
+        Cell lastEmptyCell = null;
+        for (int j = 0; j < 3; j++) {
+            final Cell cell = new Cell(j, j);
+            if (gameTable.isEmpty(cell)) {
+                lastEmptyCell = cell;
+                countEmptyCells++;
+            } else if (gameTable.getSign(cell) == sign) {
+                countSignCells++;
+            } else {
+                break;
+            }
+        }
+        if (countEmptyCells == 1 && countSignCells == 2) {
+            gameTable.setSign(lastEmptyCell, sign);
+            return true;
+        }
+        return false;
+    }
+}
